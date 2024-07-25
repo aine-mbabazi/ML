b7ad5bbdf67b0982f8a2349243ed75dc0a19b8d4
aine-mbabazi <patainembabazi@gmail.com>
2024-07-25 18:04:11 +0300

    Added heaps

---
+++ heaps.py
@@ -0,0 +1,125 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def parent(self, index):
+        return (index - 1) 
+
+    def left_child(self, index):
+        return 2 * index + 1
+
+    def right_child(self, index):
+        return 2 * index + 2
+
+    def insert(self, value):
+        self.heap.append(value)
+        self._heapify_up(len(self.heap) - 1)
+
+    def _heapify_up(self, index):
+        while index > 0 and self.heap[index] > self.heap[self.parent(index)]:
+            
+            self.heap[index], self.heap[self.parent(index)] = self.heap[self.parent(index)], self.heap[index]
+            index = self.parent(index)
+
+    def extract_max(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+    def _heapify_down(self, index):
+        largest = index
+        left = self.left_child(index)
+        right = self.right_child(index)
+
+        if left < len(self.heap) and self.heap[left] > self.heap[largest]:
+            largest = left
+        if right < len(self.heap) and self.heap[right] > self.heap[largest]:
+            largest = right
+
+        if largest != index:
+            self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
+            self._heapify_down(largest)
+
+    def peek(self):
+        return self.heap[0] if self.heap else None
+
+    def size(self):
+        return len(self.heap)
+
+
+max_heap = MaxHeap()
+max_heap.insert(10)
+max_heap.insert(80)
+max_heap.insert(90)
+max_heap.insert(20)
+max_heap.insert(5)
+print(max_heap.extract_max())  
+print(max_heap.peek())    
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def parent(self, index):
+        return (index - 1) 
+
+    def left_child(self, index):
+        return 2 * index + 1
+
+    def right_child(self, index):
+        return 2 * index + 2
+
+    def insert(self, value):
+        self.heap.append(value)
+        self._heapify_up(len(self.heap) - 1)
+
+    def _heapify_up(self, index):
+        while index > 0 and self.heap[index] < self.heap[self.parent(index)]:
+           
+            self.heap[index], self.heap[self.parent(index)] = self.heap[self.parent(index)], self.heap[index]
+            index = self.parent(index)
+
+    def extract_min(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+    def _heapify_down(self, index):
+        smallest = index
+        left = self.left_child(index)
+        right = self.right_child(index)
+
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self._heapify_down(smallest)
+
+    def peek(self):
+        return self.heap[0] if self.heap else None
+
+    def size(self):
+        return len(self.heap)
+
+
+min_heap = MinHeap()
+min_heap.insert(108)
+min_heap.insert(120)
+min_heap.insert(5)
+print(min_heap.extract_min()) 
+print(min_heap.peek())         
